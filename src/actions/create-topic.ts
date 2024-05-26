e1f6776dc4fd84fc3e90ace7c3c566205972ee00
kiran pant <kiran.r.pant@gmail.com>
2024-05-26 14:07:58 +0545

    receive form data

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,4 +1,11 @@
 "use server";
-export const createTopic = async() => {
-    //TODO:revalidate the homepage
+
+import { db } from "@/db";
+
+export const createTopic = async (formData: FormData) => {
+  const name = formData.get("name");
+  const description = formData.get("description");
+  console.log({ name, description });
+
+  //TODO:revalidate the homepage
 };
