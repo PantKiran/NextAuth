537b467001630f9d63bc1877eeaa43c05473b934
kiran pant <kiran.r.pant@gmail.com>
2024-05-26 14:45:44 +0545

    form validation with zod

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,11 +1,24 @@
 "use server";
 
 import { db } from "@/db";
-
+import { z } from "zod";
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Must be lowercase letters or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
 export const createTopic = async (formData: FormData) => {
-  const name = formData.get("name");
-  const description = formData.get("description");
-  console.log({ name, description });
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors);
+  }
 
   //TODO:revalidate the homepage
 };
