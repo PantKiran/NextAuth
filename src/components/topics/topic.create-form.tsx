b847524ff45df04e82b2bc7a0cb81f0cb2a20faa
kiran pant <kiran.r.pant@gmail.com>
2024-05-26 19:07:29 +0545

    validation error

--- src/components/topics/topic.create-form.tsx
+++ src/components/topics/topic.create-form.tsx
@@ -26,12 +26,16 @@ const TopicCreateForm = () => {
               label="Name"
               labelPlacement="outside"
               placeholder="Name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(", ")}
             />
             <Textarea
               name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Describe your topic"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(", ")}
             />
             <Button type="submit">Submit</Button>
           </div>
