8acabbe43bd95fa16606d4d6def952cf5c8ae1db
kiran pant <kiran.r.pant@gmail.com>
2024-05-29 18:52:44 +0545

    feat:topic show page

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,8 +1,19 @@
-
-const TopicShowPage = () => {
-  return (
-    <div>Topic show</div>
-  )
+interface TopicShowPageProps {
+  params: {
+    slug: string;
+  };
 }
+const TopicShowPage = ({ params }: TopicShowPageProps) => {
+  const { slug } = params;
+  console.log(slug);
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className=" col-span-3">
+        <h1 className="tex-2xl font-bold mb-2">{slug}</h1>
+      </div>
+      <div>Create New Post</div>
+    </div>
+  );
+};
 
-export default TopicShowPage
+export default TopicShowPage;
