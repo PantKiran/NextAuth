a0b0362d8229c34237b84efe28563fe2f0408e99
kiran pant <kiran.r.pant@gmail.com>
2024-05-30 15:44:40 +0545

    additional components

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+};
