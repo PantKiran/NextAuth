4e921b02312303c14653fa3d3f96e0e284d9b489
kiran pant <kiran.r.pant@gmail.com>
2024-06-03 17:41:02 +0545

    feat:add postShowPage

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -19,7 +19,7 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
         {"< "}Back to {slug}
       </Link>
-      {/* <PostShow /> */}
+      <PostShow postId={postId}/>
       {/* <CommentCreateForm postId={postId} startOpen /> */}
       {/* <CommentList comments={comments} /> */}
     </div>
